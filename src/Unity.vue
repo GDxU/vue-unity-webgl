c239e57fe04e5e501b550276e612184e9950c35b
Marcos Penna <penna@be-interactive.com.br>
2021-07-27 23:52:23 -0300

    working with Unity 2021 WebGL

--- src/Unity.vue
+++ src/Unity.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="webgl-content">
-    <div :id="containerId" v-bind:style="{ width: width + 'px', height: height + 'px' }"></div>
+    <canvas :id="containerId" v-bind:style="{ width: width + 'px', height: height + 'px' }"></canvas>
     <div v-if="loaded == false">
       <div class="unity-loader">
         <div class="bar">
-          <div class="fill" v-bind:style="{ width: progress * 100 + '%'}"></div>
+          <div class="fill" v-bind:style="{ width: progress * 100 + '%' }"></div>
         </div>
       </div>
     </div>
@@ -15,100 +15,143 @@
 </template>
 
 <script>
-  import Vue from 'vue'
+import Vue from 'vue';
 
-  export default {
-    props: ['src', 'module', 'width', 'height', 'externalProgress', 'unityLoader', 'hideFooter'],
-    name: 'UnityWebGL',
-    data () {
-      return {
-        containerId: 'unity-container-' + Number(Math.random().toString().substr(3, length) + Date.now()).toString(36),
-        gameInstance: null,
-        loaded: false,
-        progress: 0,
-        error: null
-      }
+export default {
+  props: ['src', 'module', 'width', 'height', 'externalProgress', 'unityPrefix', 'hideFooter'],
+
+  name: 'UnityWebGL',
+
+  data() {
+    return {
+      containerId: 'unity-container-' + Number(Math.random().toString().substr(3, length) + Date.now()).toString(36),
+      gameInstance: null,
+      loaded: false,
+      progress: 0,
+      error: null,
+    };
+  },
+
+  computed: {
+    unityLoader() {
+      return `${this.unityPrefix}.loader.js`
     },
-    methods: {
-      fullscreen () {
-        this.gameInstance.SetFullscreen(1)
-      },
-      message(gameObject, method, param) {
-        if (param === null) {
-          param = ''
-        }
-        if (this.gameInstance !== null){
-          this.gameInstance.SendMessage(gameObject, method, param)
-        } else {
-          console.warn('vue-unity-webgl: you\'ve sent a message to the Unity content, but it wasn\t instantiated yet.')
-        }
-      }
+    dataUrl() {
+      return `${this.unityPrefix}.data`
     },
-    beforeMount() {
-
-      if (!this.eventBus) {
-        this.eventBus = new Vue({
-          data: {
-            ready: false,
-            load: false
-          }
-        })
-      }
-
-      if (typeof UnityLoader === 'undefined' && this.unityLoader && !this.eventBus.load) {
-        const script = document.createElement('SCRIPT')
-        script.setAttribute('src', this.unityLoader)
-        script.setAttribute('async', '')
-        script.setAttribute('defer', '')
-        document.body.appendChild(script)
-        this.eventBus.load = true
-        script.onload = () => {
-          this.eventBus.ready = true
-          this.eventBus.$emit('onload')
-        }
-      } else {
-        this.eventBus.ready = true
-        this.eventBus.load = true
-      }
+    frameworkUrl() {
+      return `${this.unityPrefix}.framework.js`
     },
-    mounted () {
-      const instantiate = () => {
-        if (typeof UnityLoader === 'undefined') {
-          let error = 'The UnityLoader was not defined, please add the script tag ' +
-            'to the base html and embed the UnityLoader.js file Unity exported or use "unityLoader" attribute for path to UnityLoader.js.'
-          console.error(error)
-          this.error = error
-          return
-        }
-        if (this.src === null) {
-          let error = 'Please provice a path to a valid JSON in the "src" attribute.'
-          console.error(error)
-          this.error = error
-          return
-        }
-        let params = {}
-        if (this.externalProgress) {
-          params.onProgress = this.externalProgress
-        } else {
-          params.onProgress = ((gameInstance, progress) => {
-            this.loaded = (progress === 1)
-            this.progress = progress
-          })
-        }
-        if (this.module) {
-          params.Module = this.module
-        }
-        this.gameInstance = UnityLoader.instantiate(this.containerId, this.src, params)
-      }
-
-      if (this.eventBus.ready) {
-        instantiate()
-      } else {
-        this.eventBus.$on('onload', () => {
-          instantiate()
-        })
-      }
+    codeUrl() {
+      return `${this.unityPrefix}.wasm`
+    },
+    unityCanvas() {
+      return `#${this.containerId}`
     }
-  }
+  },
 
+  methods: {
+    fullscreen() {
+      this.gameInstance.SetFullscreen(1);
+    },
+
+    message(gameObject, method, param) {
+      if (param === null) {
+        param = '';
+      }
+      if (this.gameInstance !== null) {
+        this.gameInstance.SendMessage(gameObject, method, param);
+      } else {
+        console.warn("vue-unity-webgl: you've sent a message to the Unity content, but it wasn\t instantiated yet.");
+      }
+    },
+  },
+
+  beforeMount() {
+    if (!this.eventBus) {
+      this.eventBus = new Vue({
+        data: {
+          ready: false,
+          load: false,
+        },
+      });
+    }
+
+    if (typeof createUnityInstance === 'undefined' && this.unityLoader && !this.eventBus.load) {
+      const script = document.createElement('SCRIPT');
+      script.setAttribute('src', this.unityLoader);
+      script.setAttribute('async', '');
+      script.setAttribute('defer', '');
+      document.body.appendChild(script);
+      this.eventBus.load = true;
+      script.onload = () => {
+        this.eventBus.ready = true;
+        this.eventBus.$emit('onload');
+      };
+    } else {
+      this.eventBus.ready = true;
+      this.eventBus.load = true;
+    }
+  },
+
+  mounted() {
+    const instantiate = () => {
+      if (typeof createUnityInstance === 'undefined') {
+        let error =
+          'The createUnityInstance was not defined, please add the script tag ' +
+          'to the base html and embed the loader script file exported by Unity or use the "unityLoader" attribute for the path to the loader script file.';
+        console.error(error);
+        this.error = error;
+        return;
+      }
+
+      // if (this.src === null) {
+      //   let error = 'Please provide a path to a valid JSON in the "src" attribute.';
+      //   console.error(error);
+      //   this.error = error;
+      //   return;
+      // }
+
+      let params = {};
+      if (this.externalProgress) {
+        params.onProgress = this.externalProgress;
+      } else {
+        params.onProgress = (gameInstance, progress) => {
+          this.loaded = progress === 1;
+          this.progress = progress;
+        };
+      }
+
+      if (this.module) {
+        params.Module = this.module;
+      }
+
+      let promise = createUnityInstance(document.querySelector(this.unityCanvas), {
+        dataUrl: this.dataUrl,
+        frameworkUrl: this.frameworkUrl,
+        codeUrl: this.codeUrl,
+        streamingAssetsUrl: 'StreamingAssets',
+        companyName: 'DefaultCompany',
+        productName: 'CallJSFromCSharp',
+        productVersion: '0.1',
+      });
+
+      promise.then(
+        (result) => (this.gameInstance = result),
+        (error) => {
+          console.error(error);
+          this.error = error;
+        }
+      );
+    };
+
+    if (this.eventBus.ready) {
+      instantiate();
+    } else {
+      this.eventBus.$on('onload', () => {
+        instantiate();
+      });
+    }
+  },
+};
 </script>
